50b587daa7c0c797a5dc32d151bb296541eb2d1d
mcnakhaee <mcnakhaee@gmail.org>
2020-10-30 12:10:01 +0100

    update global vars

--- R/globals.R
+++ R/globals.R
@@ -3,4 +3,5 @@ utils::globalVariables(c("Aantal", "Datum", "Bron","Geslacht","Date_of_report",
                          "Total_reported","Hospital_admission","Deceased","LeeftijdGroep",
                          "AantalCumulatief","Perioden",
                          'Burgerlijke staat',"Regio's",'Overledenen (aantal)',"Age, 31 December",
-                         "Deaths (aantal)","Leeftijd (op 31 december)",'Regions'))
+                         "Deaths (aantal)","Leeftijd (op 31 december)",'Regions',
+                         "Country","country"))
